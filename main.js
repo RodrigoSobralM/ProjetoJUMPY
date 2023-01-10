fd873061834d244ffef794b2ddde7e1ad4cc9261
ciueiCjay <rodrigo_sobral.m@hotmail.com>
2023-01-09 22:13:34 -0300

    upload project

---
+++ main.js
@@ -0,0 +1,20 @@
+let camp = document.querySelector("#camp");
+
+function validate() {
+    
+    document.addEventListener('click', (event) => {
+        
+        if (document.querySelector('#input1').value === "" ||
+            document.querySelector('#input2').value === "" ||
+            document.querySelector('#input3').value === "" ||
+            document.querySelector('#input4').value === "" ||
+            document.querySelector('#input5').value === "") {
+                camp.innerHTML = "Campos obrigatórios não registrados.";
+                camp.style.color = "red";
+                camp.style.textAlingh = "left"
+        } else {
+            camp.innerHTML = "Sucesso!";
+            camp.style.color = "green";
+        }
+    });
+}
